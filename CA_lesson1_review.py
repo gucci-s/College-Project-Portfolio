26825e57af3c22ac74092fe4bd1ab0ea9d3bb06c
gucci-s <90578065+gucci-s@users.noreply.github.com>
2023-02-28 21:49:03 -0600

    Create CA_lesson1_review.py
    Code Academy Lesson 1 Review

---
+++ CA_lesson1_review.py
@@ -0,0 +1,13 @@
+"""S.Gucci"""
+
+my_age = 38
+half_my_age = int(my_age / 2)
+greeting = "Hello"
+name = "Sarah"
+greeting_with_name = greeting + name
+
+print(my_age)
+print(half_my_age)
+print(greeting)
+print(name)
+print(greeting_with_name)
