b67d54938ffdd04d30baea5b7242cc5992baad7e
gucci-s <90578065+gucci-s@users.noreply.github.com>
2023-02-28 22:18:23 -0600

    Create CA_initialsproject.py

---
+++ CA_initialsproject.py
@@ -0,0 +1,13 @@
+#Sarah Gucciardo
+#I started college at 36 years old
+
+initials = """
+                    S S S     G G G
+                   S     S   G     G
+                   S         G
+                    S S S    G   G G
+                         S   G      G
+                   S     S   G      G
+                    S S S     G G G  """
+
+print(initials)
