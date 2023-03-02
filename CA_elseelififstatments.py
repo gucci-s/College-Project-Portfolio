2ae095384fd334ffefce9f492bc1eb52ec6a65d1
gucci-s <90578065+gucci-s@users.noreply.github.com>
2023-03-01 20:55:23 -0600

    Create CA_elseelififstatments.py

---
+++ CA_elseelififstatments.py
@@ -0,0 +1,16 @@
+#grade report using if, elif and else statements
+#elif will check certain parameters to choose the right output
+#when the input matches a statement it stops and uses that output
+
+grade = 86
+
+if grade >= 90:
+  print("A")
+elif grade >= 80:
+  print("B")
+elif grade >= 70:
+  print("C")
+elif grade >= 60:
+  print("D")
+else:
+  print("F")
