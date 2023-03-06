548cb0999c25c20ff9a97667a3711ed6b15d7cc1
gucci-s <90578065+gucci-s@users.noreply.github.com>
2023-03-06 02:12:11 -0600

    Built in functions
    Exploring built in functions of Python.
    
    max()
    min()
    round()

---
+++ builtinFunctions.py
@@ -0,0 +1,18 @@
+tshirt_price = 9.75
+shorts_price = 15.50
+mug_price = 5.99
+poster_price = 2.00
+
+# Write your code below:
+
+max_price = max(9.75, 15.50, 5.99, 2.00)
+
+print(max_price)
+
+min_price = min(9.75, 15.50, 5.99, 2.00)
+
+print(min_price)
+
+#The round() built-in function takes in two arguments. The first argument is the number we want to round, followed by an argument on how many decimal places we want to round it.
+rounded_price = round(tshirt_price, 1)
+print(rounded_price)
